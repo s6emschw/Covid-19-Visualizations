607faa3cfab400f4f3518613b066e5fe497cc257
s6emschw <64075224+s6emschw@users.noreply.github.com>
2021-03-22 17:46:42 +0100

    Replace triple quoted strings with hashtag.

--- src/data_management/task_create_data.py
+++ src/data_management/task_create_data.py
@@ -9,14 +9,12 @@ from src.config import SRC
 @pytask.mark.depends_on(SRC / "original_data" / "policy_data.csv")
 @pytask.mark.produces(BLD / "data" / "covid_policy.csv")
 def task_create_data(depends_on, produces):
-    """Read the data"""
+    # Read the data
     policy_data = pd.read_csv(depends_on).set_index("indicator")
-    """Change datatypes
-    """
+    # Change datatypes
     for col in ["start_date", "end_date"]:
         policy_data[col] = policy_data[col].astype("datetime64[ns]")
-    """Decompose data for every single day from 2020-02-01 to 2021-02-14 for each indicator
-    """
+    # Decompose data for every single day from 2020-02-01 to 2021-02-14 for each indicator
     policy_data["row"] = range(len(policy_data))
     starts = policy_data[
         ["start_date", "scale", "flag", "recorded_flag", "maximum", "row"]
@@ -29,17 +27,14 @@ def task_create_data(depends_on, produces):
         lambda x: x.set_index("date").resample("D").fillna(method="pad")
     )
     policy_data_decomp.reset_index(inplace=True)
-    """Long to wide format
-    """
+    # Long to wide format
     covid_policy = policy_data_decomp.pivot(
         index="date",
         columns="indicator",
         values=["scale", "flag", "recorded_flag", "maximum"],
     )
     covid_policy.columns = ["_".join(col) for col in covid_policy.columns]
-    """Fill missing values
-    """
+    # Fill missing values
     covid_policy.replace({-100: np.nan}, inplace=True)
-    """Save the data
-    """
+    # Save the data
     covid_policy.to_csv(produces)
